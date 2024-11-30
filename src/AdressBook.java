fd255f466c903947bac3940604cb54d7738d195f
Dhruv <guptadhruv1908@gmail.com>
2024-11-30 11:23:12 +0530

    uc3 commit

--- src/AdressBook.java
+++ src/AdressBook.java
@@ -2,37 +2,14 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class AdressBook {
-    List<Contact> adBook=new ArrayList<>();
+public class AdressBook<T> {
+    List<T> adBook=new ArrayList<T>();
 
     public void display(){
         System.out.println(adBook);
     }
-     void createContact(AdressBook a1){
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter the firstName");;
-        String firstname=sc.next();
-
-        System.out.println("Enter the lastName");;
-        String lastname=sc.next();
-
-        System.out.println("Enter the city");;
-        String city=sc.next();
-
-        System.out.println("Enter the state");;
-        String state=sc.next();
-
-        System.out.println("Enter the mail");;
-        String email=sc.next();
-
-        System.out.println("Enter the Phone");;
-        int phone=sc.nextInt();
-
-        System.out.println("Enter the Zip");;
-        int zip=sc.nextInt();
-        Contact c1=new Contact(firstname,lastname,city,state,email,phone,zip);
-        adBook.add(c1);
-
+    public void addContact(T c1){
+        adBook.add((T) c1);
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -11,16 +11,61 @@ public class Main {
         System.out.println("Welcome");
         do{
             System.out.println("Enter 1 to create contact");
+            System.out.println("Enter 2 to display contact");
             int input=sc.nextInt();
             switch(input){
-                case 1-> a1.createContact(a1);
+                case 1-> a1.addContact(createBussinessContact());
+                case 2-> a1.display();
                 default -> System.out.println("Wrong");
             }
+
             System.out.println("Enter 0 to exit");
             i=sc.nextInt();
-        }while(i==0);
+        }while(i!=0);
         System.out.println("Thank you");
         sc.close();
+    }
+
+    public static Contact createContact(){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the firstName");;
+        String firstname=sc.next();
+
+        System.out.println("Enter the lastName");;
+        String lastname=sc.next();
+
+        System.out.println("Enter the city");;
+        String city=sc.next();
+
+        System.out.println("Enter the state");;
+        String state=sc.next();
+
+        System.out.println("Enter the mail");;
+        String email=sc.next();
+
+        System.out.println("Enter the Phone");;
+        int phone=sc.nextInt();
+
+        System.out.println("Enter the Zip");;
+        int zip=sc.nextInt();
+        Contact c1=new Contact(firstname,lastname,city,state,email,phone,zip);
+        return c1;
+
+    }
+
+    public static BussinessContact createBussinessContact(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Company name ");
+        String cname = sc.next();
+        System.out.println("Employee Name");
+        String ename = sc.next();
+        System.out.println("Employee mail");
+        String bmail= sc.next();
+
+        BussinessContact b1 = new BussinessContact(cname,ename,bmail);
+        return b1;
+
+
     }
 
 
